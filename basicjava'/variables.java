4244453d80723a1aaa6563d2d5d856086f6137c4
Nishantunhale <nishantunhale2420@gmail.com>
2026-09-22 19:16:02 +0530

    connected with repo

---
+++ basicjava'/variables.java
@@ -0,0 +1,9 @@
+public class variables{
+    public static void main(String args[]){
+        int a=5, b=5;
+        //   c =5.4 , d=4.3;
+        String names="nishh";
+        System.out.println("integers a="+(a)"+b="+(b));
+    }
+    
+}
